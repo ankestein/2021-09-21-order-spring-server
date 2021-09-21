fb73c2df21cf995f54a8688aa5ba52107b63136b
Anke <a.stein@posteo.de>
2021-09-21 20:26:17 +0200

    Correct creation of new ArrayList; create getProductByName; other minor changes

--- src/main/java/de/neuefische/orderspringserver/repo/ProductRepo.java
+++ src/main/java/de/neuefische/orderspringserver/repo/ProductRepo.java
@@ -1,29 +1,44 @@
 package de.neuefische.orderspringserver.repo;
 
 import de.neuefische.orderspringserver.model.Product;
+import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductRepo {
-    private final List<Product> products;
+
+    private List<Product> products = new ArrayList<>();
 
     public ProductRepo(List<Product> products) {
-        this.products = Collections.unmodifiableList(products);
+        this.products = products;
     }
 
     public List<Product> listProducts() {
         return products;
     }
 
-    public Optional<Product> getProduct(String id) {
+    public Optional<Product> getProductById(String id) {
         for (Product product : products) {
             if (product.getId().equals(id)) {
                 return Optional.of(product);
             }
         }
         return Optional.empty();
+     }
 
+    public List<Product> getProductByName(String search) {
+        List<Product> productsWithMatchingName = new ArrayList<>();
+        for (Product product : products) {
+            if (product.getName().equals(search)) {
+                productsWithMatchingName.add(product);
+            }
+        }
+        return productsWithMatchingName;
     }
+
+
 }
