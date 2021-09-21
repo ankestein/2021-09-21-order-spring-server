795dcd84df4c994333424981f33a02cbbaa8a3f5
Anke <a.stein@posteo.de>
2021-09-21 21:05:15 +0200

    Change/add strings in @RequestMapping

--- src/main/java/de/neuefische/orderspringserver/controller/OrderController.java
+++ src/main/java/de/neuefische/orderspringserver/controller/OrderController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("orders")
 public class OrderController {
 
     private final OrderService orderService;
