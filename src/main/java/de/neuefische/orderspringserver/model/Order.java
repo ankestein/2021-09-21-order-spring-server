c4e9ba6173318426d89d8476fc713a6da954e5db
Anke <a.stein@posteo.de>
2021-09-21 16:46:58 +0200

    Create Order and Product classes

--- src/main/java/de/neuefische/orderspringserver/model/Order.java
+++ src/main/java/de/neuefische/orderspringserver/model/Order.java
@@ -1,4 +1,43 @@
 package de.neuefische.orderspringserver.model;
 
+import java.util.Objects;
+
 public class Order {
+
+    private String id;
+    private String name;
+
+    public Order(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id) && Objects.equals(name, order.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
--- src/main/java/de/neuefische/orderspringserver/model/Product.java
+++ src/main/java/de/neuefische/orderspringserver/model/Product.java
@@ -1,4 +1,42 @@
 package de.neuefische.orderspringserver.model;
 
+import java.util.Objects;
+
 public class Product {
+    private String id;
+    private String name;
+
+    public Product(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
