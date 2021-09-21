64130cc54fb0eb73d0429fbf25a31da902445744
Anke <a.stein@posteo.de>
2021-09-21 21:01:44 +0200

    Create OrderController

--- src/main/java/de/neuefische/orderspringserver/controller/OrderController.java
+++ src/main/java/de/neuefische/orderspringserver/controller/OrderController.java
@@ -1,4 +1,30 @@
 package de.neuefische.orderspringserver.controller;
 
+import de.neuefische.orderspringserver.model.Order;
+import de.neuefische.orderspringserver.model.Product;
+import de.neuefische.orderspringserver.service.OrderService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
 public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public List<Order> listOrders(){
+        return orderService.listOrders();
+    }
+
+    @PutMapping
+    public Order orderProducts(@RequestBody List<String> productIds) {
+        return orderService.orderProducts(productIds);
+    }
+
 }
