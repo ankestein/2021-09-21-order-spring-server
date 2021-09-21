2bd5bc5ebc7e8fbcd18eaad2e606b844daf32693
Anke <a.stein@posteo.de>
2021-09-21 20:26:43 +0200

    Create ProductService and OrderService

--- src/main/java/de/neuefische/orderspringserver/service/OrderService.java
+++ src/main/java/de/neuefische/orderspringserver/service/OrderService.java
@@ -1,4 +1,41 @@
 package de.neuefische.orderspringserver.service;
 
+import de.neuefische.orderspringserver.model.Order;
+import de.neuefische.orderspringserver.model.Product;
+import de.neuefische.orderspringserver.repo.OrderRepo;
+import de.neuefische.orderspringserver.repo.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class OrderService {
+
+    private final OrderRepo orderRepo;
+    private final ProductService productService;
+
+    @Autowired
+    public OrderService(OrderRepo orderRepo, ProductService productService) {
+        this.orderRepo = orderRepo;
+        this.productService = productService;
+    }
+
+    public List<Order> listOrders(){
+        return orderRepo.listOrders();
+    }
+
+    public Order orderProducts(List<String> productIds) {
+        List<Product> productsToOrder = new ArrayList<>();
+        for (String productId : productIds) {
+            Product productToAdd = productService.getProductById(productId);
+            productsToOrder.add(productToAdd);
+        }
+        String id = UUID.randomUUID().toString();
+        Order order = new Order(id, productsToOrder);
+        return orderRepo.addOrder(order);
+    }
+
 }
--- src/main/java/de/neuefische/orderspringserver/service/ProductService.java
+++ src/main/java/de/neuefische/orderspringserver/service/ProductService.java
@@ -1,4 +1,37 @@
 package de.neuefische.orderspringserver.service;
 
+import de.neuefische.orderspringserver.model.Order;
+import de.neuefische.orderspringserver.model.Product;
+import de.neuefische.orderspringserver.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class ProductService {
+
+    private final ProductRepo productRepo;
+
+    @Autowired
+    public ProductService(ProductRepo productRepo) {
+        this.productRepo = productRepo;
+    }
+
+    public List<Product> list() {
+        return productRepo.listProducts();
+    }
+
+    public Product getProductById(String id) {
+        Optional<Product> optionalProduct = productRepo.getProductById(id);
+        if (optionalProduct.isPresent()) {
+            return optionalProduct.get();
+        } else {
+            throw new IllegalArgumentException("Product with ID " + id + " not found");
+        }
+    }
+
 }
